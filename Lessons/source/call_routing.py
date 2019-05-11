de508992647ad7b40445bd20179cf8a1f438a22f
Nathan Pillai <natepill@gmail.com>
2019-05-11 16:44:37 -0700

    [added] Search for Trie Tree

--- Lessons/source/call_routing.py
+++ Lessons/source/call_routing.py
@@ -1,9 +1,10 @@
 # creating Trieclass
 
 class TrieNode(object):
-    def __init__(self, root=None):
+    def __init__(self):
         """Initializing the node with list of 10 digits from 0-9 and store price"""
-        # self.digit = digit
+        self.digit = 0
+
         # initializing 10 children for each node because there are 10 digits possible
         self.children = [None] * 10
         #
@@ -12,12 +13,23 @@ class TrieNode(object):
 
         # to indicate we traverse all the digits in the route
         self.end_path = False
+
+
         # consider having store the len of the route
 
+    def __repr__(self):
+        """Return a string representation of this trie node."""
+        return 'TrieNode({!r})'.format(self.children)
+
 class TrieTree(object):
-    def __init__(self):
-        self.root = TrieNode('+')
+
+    def __init__(self, routes=None):
+        """Initialize trie tree with all routes"""
+        self.root = TrieNode()
         self.size = 0
+        if routes != None:
+            for route, price in routes.items():
+                self.add(route, price)
 
     def __repr__(self):
         "return A string represention of the Trie tree"
@@ -25,27 +37,55 @@ class TrieTree(object):
 
     def add(self, route_number, price):
         """Add the new digit as node"""
+
         node = self.root
-        print('self.root', self.root)
+        print("root: ", node)
+        for index, digit in enumerate(route_number):
 
-        for index in range(1, len(route_number)):
-            value = int(route_number[index])
-            print("node: ",node)
-            print(node.children)
-            if node.children[value] == None:
-                # assinging the children as new node
-                node.children[value] == TrieNode()
+            if node.children[int(digit)] == None:
+                node.children[int(digit)] = TrieNode()
+                node.digit = int(digit)
+
                 self.size += 1
-            # updating the node
-            node = node.children[value]
-        # attaching the price to last node
-        node.price = price
+                # print(node.digit)
+                node.price = price
+            # if we hit the last digit in route we break out of loop don't update node
 
+            if index == len(route_number)-1:
+                node.end_path = True
+                break
+            # updating the node
+            node = node.children[int(digit)]
+
+
+        # print("last node: {}, node.digit: {} price: {}".format(node, node.digit, node.price))
+    def search(self, phone_number):
+        """Return a price for givin phone number searching through Trie structured routes"""
+        # start at the root
+        node = self.root
+        price = 0
+        for digit in phone_number:
+            # check if node exists where digit equals index
+            if node.children[int(digit)] != None:
+                price = node.price
+                node = node.children[int(digit)]
+            else: # first unmatch digit and we break
+                break
+        # return the price of last node
+        return price
 
 if __name__ == "__main__":
-    route = '+1512'
+    route = '1415'
     price = '0.04'
-
+    price_longer = '0.02'
+    route_longer = '1415234'
+    phone_number = '14152345678'
     obj = TrieTree()
-    obj.add(route, price)
+    obj.add(route_longer, price_longer)
+    # obj.add(route, price)
+
     print(obj)
+
+    # Testing search
+
+    print(obj.search(phone_number))
