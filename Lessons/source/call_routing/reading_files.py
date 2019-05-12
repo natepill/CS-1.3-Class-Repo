a62e42224d5defc56304dbf62c339ced305dd616
Nathan Pillai <natepill@gmail.com>
2019-05-11 17:26:55 -0700

    [removed] dict comprehension

--- Lessons/source/call_routing/reading_files.py
+++ Lessons/source/call_routing/reading_files.py
@@ -7,9 +7,20 @@ def reading_route(file_name):
     dictionary {route#: cost}
     '''
     with open(file_name) as f:
-        d = dict(line.strip().split(',') for line in f)
-    return d
+    #     d = dict(line.strip().split(',') for line in f)
+    # return d
 
+        dict_of_routes = dict()
+
+        for line in f:
+            pair = line.strip().split(',')
+            # pair[0] = pair[0].replace('+', '')
+            # dict_of_routes[pair[0]] = pair[1]
+
+            dict_of_routes[pair[0][1:]] = pair[1]
+            # print(pair)
+
+    return dict_of_routes
 
 def reading_phone(file_name):
     '''
@@ -28,10 +39,10 @@ def calculate_call_cost(list_of_phone_numbers, cost_of_routes):
 
 
 if __name__ == "__main__":
-    route_path = "project/data/route-costs-4.txt"
-    phone_path = "project/data/phone-numbers-3.txt"
+    route_path = "route-costs-10.txt"
+    # phone_path = "project/data/phone-numbers-3.txt"
 
     call_costs = reading_route(route_path)
     print(call_costs)
-    phone_numbers = reading_phone(phone_path)
-    print(phone_numbers)
+    # phone_numbers = reading_phone(phone_path)
+    # print(phone_numbers)
