c1a7328261dbd876f2567e8e4b10ff6acd4395eb
Nathan Pillai <natepill@gmail.com>
2019-05-14 15:56:14 -0700

    [added] load routes

--- Lessons/source/call_routing/call_routing.py
+++ Lessons/source/call_routing/call_routing.py
@@ -1,97 +1,66 @@
-# creating Trieclass
-
-class TrieNode(object):
-    def __init__(self):
-        """Initializing the node with list of 10 digits from 0-9 and store price"""
-        self.digit = 0
-
-        # initializing 10 children for each node because there are 10 digits possible
-        self.children = [None] * 10
-        #
-        self.price = 0
+# reading the file
+from trie_tree import TrieTree
 
 
-        # to indicate we traverse all the digits in the route
-        self.end_path = False
+def load_routes(file_name):
+    '''
+    Reading the route costs from the text file and return
+    dictionary {route#: cost}
+    '''
+    with open(file_name) as f:
+
+        for line in f:
+            pair = line.strip().split(',')
+            route = pair[0].replace('+', '')
+            price = pair[1]
+
+            route_and_price = [route, price]
+            print(route_and_price)
+
+            route_tree = TrieTree(route_and_price)
 
 
-        # consider having store the len of the route
-
-    def __repr__(self):
-        """Return a string representation of this trie node."""
-        return 'TrieNode({!r})'.format(self.children)
-
-class TrieTree(object):
-
-    def __init__(self, routes=None):
-        """Initialize trie tree with all routes"""
-        self.root = TrieNode()
-        self.size = 0
-        if routes != None:
-            for route, price in routes.items():
-                self.add(route, price)
-                self.size += 1
-
-    def __repr__(self):
-        "return A string represention of the Trie tree"
-        return 'size: {}'.format(self.size)
-
-    def add(self, route_number, price):
-        """Add the new digit as node"""
-
-        node = self.root
-        # print("root: ", node)
-        for index, digit in enumerate(route_number):
-
-            if node.children[int(digit)] == None:
-                node.children[int(digit)] = TrieNode()
-                node.digit = int(digit)
-
-                # self.size += 1
-                # print(node.digit)
-
-            # if we hit the last digit in route we break out of loop don't update node
-
-            if index == len(route_number)-1:
-                node.price = price
-                node.end_path = True
-                break
-            # updating the node
-            node = node.children[int(digit)]
+    # return route_tree
 
 
-        print("last node: {}, node.digit: {} price: {}".format(node, node.digit, node.price))
-    def search(self, phone_number):
-        """Return a price for givin phone number searching through Trie structured routes"""
-        # start at the root
-        node = self.root
-        price = 0
-        for digit in phone_number:
-            # check if node exists where digit equals index
-            if node.children[int(digit)] != None:
-                price = node.price
-                print("each node price:", node.price)
+def reading_phone_numbers(file_name):
+    '''
+    Reading the phone numbers  from the text file and return
+    list of phone numbers
+    '''
+    with open(file_name) as f:
+        # remove the '+' sign and reads new line
+        f = f.read().replace('+', '').split('\n')
+    return f
 
-                node = node.children[int(digit)]
-            else: # first unmatch digit and we break
-                print("else each node price:", node.price)
-                break
-        # return the price of last node
-        # print(node.price)
-        return price
+
+def call_costs():
+    """Return a price for each phone number"""
+    phone_number, price = 0, 0
+    call_cost = [phone_number, price]
+
+    # file_paths
+    route_costs_4 = "data/route-costs-10.txt"
+    phone_numbers_3 = "data/phone-numbers-10.txt"
+    # route_costs_10 = "project/data/route-costs-100.txt"
+    # phone_numbers_10 = "project/data/phone-numbers-100.txt"
+
+
+    # read routes and phone numbers
+    route_costs = load_routes(route_costs_4)
+
+    print(route_costs.size)
+
+    # create a trie with all the routes
+    # route = TrieTree(route_costs)
+    # # read phone numbers
+    # phone_numbers = reading_phone_numbers(phone_numbers_3)
+    # phone_num_price = route.search("19876543210")
+    # print("size of trie: ", route.size)
+    # print(phone_num_price)
+    # return call_cost
+
+
 
 if __name__ == "__main__":
-    route = '1512'
-    price = '0.04'
-
-    price_longer = '0.01'
-    route_longer = '1415246'
-
-    phone_number = '15124156620'  # return 0.04
-    # phone_num = ''
-    obj = TrieTree({route_longer: price_longer, route: price})
-
-
-    # Testing search
-
-    print(obj.search(phone_number))
+    call_costs()
